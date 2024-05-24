e958357bda71e9c477e67f4e8b9409386f108358
Fox <zoelliotmitong@gmail.com>
2024-05-24 21:58:40 +0200

    Setup mod

---
+++ src/main/java/fr/thefox580/smpu_misc/SMPU_Misc_Fabric.java
@@ -0,0 +1,17 @@
+package fr.thefox580.smpu_misc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SMPU_Misc_Fabric implements ModInitializer {
+
+    public static final String MOD_ID = "smpu_misc";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/fr/thefox580/smpu_misc/SMPU_Misc_FabricClient.java
@@ -0,0 +1,11 @@
+package fr.thefox580.smpu_misc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SMPU_Misc_FabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
